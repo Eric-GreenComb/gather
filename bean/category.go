bfdf3410d0664ed5dcbab1fab4d707e3d5765558
eric-greencomb <1121453646@qq.com>
2016-03-16 12:23:29 +0800

    add cat attr

--- bean/category.go
+++ bean/category.go
@@ -2,16 +2,24 @@ package bean
 
 import ()
 
+// Attributes:
+//  - ID
+//  - Name
+//  - Desc
+//  - Subcategories
 type Category struct {
 	ID            string        `json:"id"`
 	Name          string        `json:"name"`
+	Desc          string        `json:"desc"`
 	Subcategories []SubCategory `json:"subcategories"`
 }
 
 // Attributes:
 //  - ID
 //  - Name
+//  - Desc
 type SubCategory struct {
 	ID   string `json:"id"`
 	Name string `json:"name"`
+	Desc string `json:"desc"`
 }
