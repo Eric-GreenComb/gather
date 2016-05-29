139c928bc9dddda9723c8941fb7c538bc4bdc469
eric-greencomb <1121453646@qq.com>
2016-05-30 00:12:15 +0800

    change send email service

--- service/email.go
+++ service/email.go
@@ -1,23 +1,59 @@
 package service
 
 import (
-	"github.com/banerwai/gather/gateway/command"
-
+	"encoding/json"
+	"flag"
 	"github.com/banerwai/global/bean"
+	"github.com/nats-io/nats"
 )
+
+var (
+	NatsUrls string
+)
+
+func init() {
+	var urls = flag.String("s", nats.DefaultURL, "The nats server URLs (separated by comma)")
+	flag.Parse()
+	NatsUrls = *urls
+}
 
 type EmailService struct {
 }
 
-func (self *EmailService) SendEmail(key string, email bean.Email) error {
-	var _service command.EmailService
-	_err := _service.LPush2Redis(key, email)
-	if _err != nil {
-		return _err
+func (self *EmailService) SendEmail(email bean.Email) error {
+	nc, err := nats.Connect(NatsUrls)
+	defer nc.Close()
+
+	if err != nil {
+		return err
 	}
-	_err = _service.Send2Gearman(key)
-	if _err != nil {
-		return _err
+
+	b, err := json.Marshal(email)
+	if err != nil {
+		return err
 	}
+
+	nc.Publish("mail", b)
+	nc.Flush()
+
+	return nil
+}
+
+func (self *EmailService) SendTpl(email_extra bean.EmailExtra) error {
+	nc, err := nats.Connect(NatsUrls)
+	defer nc.Close()
+
+	if err != nil {
+		return err
+	}
+
+	b, err := json.Marshal(email_extra)
+	if err != nil {
+		return err
+	}
+
+	nc.Publish("tpl", b)
+	nc.Flush()
+
 	return nil
 }
--- service/email_test.go
+++ service/email_test.go
@@ -6,23 +6,48 @@ import (
 	"github.com/banerwai/global/bean"
 )
 
-func TestEmailServiceSendEmail(t *testing.T) {
+// func TestEmailServiceSendEmail(t *testing.T) {
 
-	var _email EmailService
+// 	var _email EmailService
 
-	_email_bean := bean.Email{}
-	// change email
-	_email_bean.Host = "smtp.126.com:25"
-	_email_bean.User = "xxx@126.com"
-	_email_bean.Password = "xxx"
-	_email_bean.To = "xxx@126.com"
-	_email_bean.Subject = "Hi"
-	_email_bean.Body = "This is a test email"
-	_email_bean.Mailtype = "html"
+// 	_email_bean := bean.Email{}
+// 	// change email
+// 	_email_bean.Host = "smtp.126.com:25"
+// 	_email_bean.User = "xxx@126.com"
+// 	_email_bean.Password = "xxx"
+// 	_email_bean.To = "xxx@126.com"
+// 	_email_bean.Subject = "Hi"
+// 	_email_bean.Body = "This is a test email"
+// 	_email_bean.Mailtype = "html"
 
-	_err := _email.SendEmail("banerwai:email:activeuser", _email_bean)
+// 	_err := _email.SendEmail(_email_bean)
+
+// 	if _err != nil {
+// 		t.Errorf("SendEmail error")
+// 	}
+// }
+
+func TestEmailServiceSendTpl(t *testing.T) {
+	var _service EmailService
+
+	_email_extra := bean.EmailExtra{}
+	_email_extra.Email.Host = "smtp.126.com:25"
+	_email_extra.Email.User = "ministor@126.com"
+	_email_extra.Email.Password = "xxxxxx"
+	_email_extra.Email.To = "ministor@126.com"
+	_email_extra.Email.Subject = "this is a tpl email"
+	_email_extra.Email.Mailtype = "html"
+
+	_email_extra.TempName = "hello"
+
+	_map_parse := make(map[string]string)
+	_map_parse["Hi"] = "Hello"
+	_map_parse["Name"] = "Eric"
+	_email_extra.Parse = _map_parse
+
+	_err := _service.SendTpl(_email_extra)
 
 	if _err != nil {
-		t.Errorf("SendEmail error")
+		t.Errorf("SendTpl error")
 	}
 }
